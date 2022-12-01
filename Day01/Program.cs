854ab6031a0447a3cb25d15c7d88751473ac6ccb
meduzaxx <david.bohunovskyy@gmail.com>
2022-12-01 21:37:06 +0100

    Day 1

---
+++ Day01/Program.cs
@@ -0,0 +1,12 @@
+﻿string trainDataPath = @"TestFiles\trainData.txt";
+string testDataPath = @"TestFiles\testData.txt";
+
+var lines = File
+    .ReadAllText(testDataPath)
+    .Split("\r\n\r\n")
+    .ToList();
+
+var sums = lines.Select(line => line.Split("\r\n").Select(int.Parse).Sum()).ToList();
+
+Console.WriteLine($"Part 1: {sums.MaxBy(x => x)}");
+Console.WriteLine($"Part 2: {sums.OrderByDescending(x => x).Take(3).Sum()}");
