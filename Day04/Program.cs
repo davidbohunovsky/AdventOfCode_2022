3a7a21aa74e455c90c33b63b889c48d5784e2870
meduzaxx <david.bohunovskyy@gmail.com>
2022-12-04 11:07:30 +0100

    Day 4

---
+++ Day04/Program.cs
@@ -0,0 +1,27 @@
+﻿string trainDataPath = @"TestFiles\trainData.txt";
+string testDataPath = @"TestFiles\testData.txt";
+
+var lines = File
+    .ReadAllLines(testDataPath)
+    .Select(x => x.Split(","));
+
+var contains = 0;
+var overlaps = 0;
+
+foreach (var line in lines)
+{
+    var firstElf = line[0].Split("-").Select(int.Parse).ToArray();
+    var secondElf = line[1].Split("-").Select(int.Parse).ToArray();
+
+    var firstStorage = Enumerable.Range(firstElf[0], firstElf[1] - firstElf[0] + 1).ToArray();
+    var secondStorage = Enumerable.Range(secondElf[0], secondElf[1]- secondElf[0] + 1).ToArray();
+
+    if (firstStorage.All(s1 => secondStorage.Contains(s1)) || secondStorage.All(s2 => firstStorage.Contains(s2)))
+        contains++;
+
+    if (firstStorage.Any(s1 => secondStorage.Contains(s1)) || secondStorage.Any(s2 => firstStorage.Contains(s2)))
+        overlaps++;
+}
+
+Console.WriteLine($"Part1: {contains}");
+Console.WriteLine($"Part2: {overlaps}");
